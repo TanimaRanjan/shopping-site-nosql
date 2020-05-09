d9fa8016a99c425512e4003165046aee0e392d6d
TanimaRanjan <tanimaranjan@gmail.com>
2020-05-09 17:42:05 -0400

    Adding product details

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,7 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/add-product => GET
 router.get('/products', adminController.getProducts);
 
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
