ddc43267731d64dadf80786c1ba0f227d1d35eeb
TanimaRanjan <tanimaranjan@gmail.com>
2020-05-16 15:30:21 -0400

    Adding login page

--- app.js
+++ app.js
@@ -3,36 +3,60 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
+
 const errorController = require('./controllers/error');
-
-
 const User = require('./models/user');
 
+const db = require('./config/keys').mongoURI
+const secret = require('./config/keys').secretOrKey
+
 const app = express();
+const store = new MongoDBStore({
+    uri:db,
+    collection:'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth')
  
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    session({
+        secret:secret,
+        resave:false,
+        saveUninitialized:false,
+        store:store
+    })
+)
+
 app.use((req,res, next) => {
-    User.findById('5ebd8cc23f9b581890edda09').then(user => {
+    if(!req.session.user) {
+        return next()
+    }
+    
+    User.findById(req.session.user._id
+    ).then(user => {
         // req.user = new User(user.name, user.email, user.cart, user._id)
         req.user = user
         next()
     }).catch(error => console.log(error))
 })
 
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+app.use(authRoutes)
 app.use(errorController.get404);
 
-const db = require('./config/keys').mongoURI
+
 
 mongoose.connect(db, { useNewUrlParser: true } )
 .then((result) => {
