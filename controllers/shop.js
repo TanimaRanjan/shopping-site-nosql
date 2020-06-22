e61a643299b0e58c8bcd5c4787af52d6abd02717
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-22 12:59:30 -0400

    Pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,19 +6,52 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/products');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
-    .then(products => {
-      console.log(products);
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
-    })
-    .catch(err => {
-      console.log(err);
+  .countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
+  })
+  .then(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'Products',
+      path: '/products',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPrevPage: page > 1,
+      nextPage: page + 1,
+      prevPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
+  })
+  .catch(err => {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  });
+
+
+  // Product.find()
+  //   .then(products => {
+  //     console.log(products);
+  //     res.render('shop/product-list', {
+  //       prods: products,
+  //       pageTitle: 'All Products',
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
 };
 
 exports.getProduct = (req, res, next) => {
@@ -36,21 +69,38 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
 
-  console.log('REQ ', req.session) 
+  const page = +req.query.page || 1
+  let totalItems 
+  Product
+    .find()
+    .countDocuments()
+    .then(numOfProd => {
+      totalItems = numOfProd
+      console.log(totalItems)
+      return Product.find()
+      .skip((page-1)*ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE)
+      .then(products => {
+        // console.log(products)
+        res.render('shop/index', {
+          prods: products,
+          pageTitle: 'Shop',
+          path: '/',
+          // isAuthenticated: req.session.isLoggedIn, 
+          // csrfToken:req.csrfToken()
+          // totalProducts:totalItems,
+          currentPage: page,
+          hasNextPage : ITEMS_PER_PAGE * page < totalItems,
+          hasPrevPage : page > 1,
+          nextPage : page+1,
+          prevPage: page - 1,
+          lastPage: Math.ceil(totalItems/ITEMS_PER_PAGE)
 
-  Product.find()
-    .then(products => {
-      res.render('shop/index', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        // isAuthenticated: req.session.isLoggedIn, 
-        // csrfToken:req.csrfToken()
-      });
-    })
-    .catch(err => {
+        });
+      })
+  }).catch(err => {
       console.log(err);
-    });
+   });
 };
 
 exports.getCart = (req, res, next) => {
